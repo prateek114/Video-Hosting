4110729f17e55a1ccbf9d4aa23f1d2c8c7d90667
Prateek Sharma <prateeksharma1122111@gmail.com>
2023-12-27 18:08:05 +0530

    add refreshAccessToken controller

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,48 +1,28 @@
 import { Router } from "express";
-import {loginUser, logoutUser, registerUser} from "../controllers/user.controller.js"
+import {loginUser, logoutUser, registerUser,refreshAccessToken} from "../controllers/user.controller.js"
 import {upload} from "../middlewares/multer.middleware.js"
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
-// const router=Router()
-
-// router.route("/register").post(
-//     upload.fields([
-//         {
-//             name:"avatar",
-//             maxCount:1
-//         },
-//         {
-//             name:"coverImage",
-//             maxCount:1
-//         }
-//     ]),
-//     registerUser
-// )
-
-// router.route("/login").post(loginUser)
-
-// //Secured routes
-// router.route("/logout").post(verifyJWT,logoutUser)
-
-const router = Router()
+const router=Router()
 
 router.route("/register").post(
     upload.fields([
         {
-            name: "avatar",
-            maxCount: 1
-        }, 
+            name:"avatar",
+            maxCount:1
+        },
         {
-            name: "coverImage",
-            maxCount: 1
+            name:"coverImage",
+            maxCount:1
         }
     ]),
     registerUser
-    )
+)
 
 router.route("/login").post(loginUser)
 
-//secured routes
-router.route("/logout").post(verifyJWT,  logoutUser)
+//Secured routes
+router.route("/logout").post(verifyJWT,logoutUser)
+router.route("/refresh-token").post(refreshAccessToken)
 
 export default router

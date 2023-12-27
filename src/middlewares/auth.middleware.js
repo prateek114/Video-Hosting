fab64535afff1dd56b227009d785367a7b3ed048
Prateek Sharma <prateeksharma1122111@gmail.com>
2023-12-27 17:15:37 +0530

    Fix some bugs in login and logout controllers

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,29 +1,79 @@
+// import { ApiError } from "../utils/ApiError.js";
+// import { asyncHandler } from "../utils/asyncHandler.js";
+// import { User } from "../models/user.model.js";
+// import jwt from "jsonwebtoken";
+
+// export const verifyJWT=asyncHandler(async(req,_,next)=>{
+//     try {
+//         const token=req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ","")
+    
+//         if(!token){
+//             throw new ApiError(401,"Unauthorized request")
+//         }
+    
+//         const decodedToken=jwt.verify(token,process.env.ACCESS_TOKEN_SECRET)
+    
+//         const user=await User.findById(decodedToken?._id).select("-password -refreshToken")
+    
+//         if(!user){
+//             // Todo
+//             throw new ApiError(401,"Invalid Access Token")
+//         }
+    
+//         req.user=user;
+//         next()
+
+//     } catch (error) {
+//         throw new ApiError(401, error?.message || "Invalid access token")
+//     }
+// })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 import { ApiError } from "../utils/ApiError.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
-import jwt from "jsonwebtoken";
+import jwt from "jsonwebtoken"
 import { User } from "../models/user.model.js";
 
-export const verifyJWT=asyncHandler(async(req,_,next)=>{
+export const verifyJWT = asyncHandler(async(req, _, next) => {
     try {
-        const token=req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ","")
-    
-        if(!token){
-            throw new ApiError(404,"Unauthorized request")
+        const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ", "")
+        
+        console.log(token);
+        if (!token) {
+            throw new ApiError(401, "Unauthorized request")
         }
     
-        const decodeToken=jwt.verify(token,process.env.ACCESS_TOKEN_SECRET)
+        const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
     
-        const user=await User.findById(decodeToken?._id).select("-password -refreshToken")
+        const user = await User.findById(decodedToken?._id).select("-password -refreshToken")
     
-        if(!user){
-            // Todo frontend - next
-            throw new ApiError(401,"Invalid Access Token")
+        if (!user) {
+            
+            throw new ApiError(401, "Invalid Access Token")
         }
     
-        req.user=user;
+        req.user = user;
         next()
     } catch (error) {
-        throw new ApiError(401,error?.message || "Invalid access token")
+        throw new ApiError(401, error?.message || "Invalid access token")
     }
-
+    
 })
